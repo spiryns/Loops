3db7233a938351e57052a385217c6e555dca6953
spiryns <sam.piryns@ugent.be>
2025-05-14 18:16:54 +0200

    Update input_verification.ino

--- code/arduino/input_verification/input_verification.ino
+++ code/arduino/input_verification/input_verification.ino
@@ -1,9 +1,17 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>
+#include "I2Cdev.h"
+#include "MPU6050.h"
+MPU6050 mpu;
 
+void setup(){
+  Wire.begin();
+  Serial.begin(115200);
+  mpu.initialize();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop(){
+  int16_t ax, ay, az, gx, gy, gz;
+  mpu.getMotion6(&ax,&ay,&az,&gx,&gy,&gz);
+  Serial.print(ax); Serial.print(","); Serial.println(ay);
+  delay(200);
 }
